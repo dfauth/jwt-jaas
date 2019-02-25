24c651bc8390d223ce1c175b04810c8cd028ab0d
Damian Fauth <dfauth@gmail.com>
2019-02-26 05:45:08 +0800

    get with parameter testcase

--- rest/src/test/scala/com/github/dfauth/jwt_jaas/DependencyInjectionSpec.scala
+++ rest/src/test/scala/com/github/dfauth/jwt_jaas/DependencyInjectionSpec.scala
@@ -13,7 +13,7 @@ import scala.concurrent.duration._
 
 class DependencyInjectionSpec extends FlatSpec with Matchers with LazyLogging {
 
-  "any authenticated get endpoint" should "be able to propagate its user information though a pipeline of chained functions" in {
+  "any authenticated get endpoint" should "be able to execute a function taking the user as well as a parameter" in {
 
     import TestUtils._
     import akka.http.scaladsl.server.Directives._
@@ -52,7 +52,7 @@ class DependencyInjectionSpec extends FlatSpec with Matchers with LazyLogging {
     }
   }
 
-  "any authenticated post endpoint" should "be able to propagate its user information though a pipeline of chained functions" in {
+  "any authenticated post endpoint" should "be able to execute a function taking the user as well as a parameter" in {
 
     import TestUtils._
     import akka.http.scaladsl.server.Directives._
@@ -60,9 +60,7 @@ class DependencyInjectionSpec extends FlatSpec with Matchers with LazyLogging {
     import com.github.dfauth.jwt_jaas.Routes._
     import spray.json._
 
-    val compose:User => Payload => Result[Int] = {
-      user => (p:Payload) => Result(p.payload.toInt)
-    }
+    val compose:User => Payload => Result[Int] = user => (p:Payload) => Result(p.payload.toInt)
 
     val routes:Route = login(handle) ~ genericPostEndpoint(compose)
 
@@ -90,6 +88,69 @@ class DependencyInjectionSpec extends FlatSpec with Matchers with LazyLogging {
     } finally {
       endPoint.stop(bindingFuture)
     }
+  }
+
+  "any authenticated post endpoint" should "be able to propagate its user information though a pipeline of chained functions" in {
+
+    import TestUtils._
+    import akka.http.scaladsl.server.Directives._
+    import com.github.dfauth.jwt_jaas.JsonSupport._
+    import com.github.dfauth.jwt_jaas.Routes._
+    import spray.json._
+
+    val cache = Map("fred" -> 2.0)
+    val routes:Route = login(handle) ~ genericPostEndpoint(compose(cache))
+
+    val endPoint = RestEndPointServer(routes)
+    implicit val loginEndpoint:String = endPoint.endPointUrl("login")
+    val bindingFuture = endPoint.start()
+
+    Await.result(bindingFuture, 5.seconds)
+
+    try {
+      val userId:String = "fred"
+      val password:String = "password"
+      val tokens:Tokens = asUser(userId).withPassword(password).login
+
+      val payload = "2"
+      val bodyContent:String = Payload(payload).toJson.prettyPrint
+
+      val response:Response = tokens.when.log().all().
+        contentType(ContentType.JSON).
+        body(bodyContent).
+        post(endPoint.endPointUrl("endpoint"))
+
+      response.then().log.all.statusCode(200).
+        body("result",equalTo( (cache(userId)*1000).toInt*payload.toInt))
+    } finally {
+      endPoint.stop(bindingFuture)
+    }
+  }
+
+  def compose(cache:Map[String, Double]):User => Payload => Result[Int] = {
+        val extractPayload:Payload => String  = { p =>
+          logger.info(s"extractPayload: ${p}")
+          p.payload
+        }
+        val toInt:String => Int = { s =>
+          logger.info(s"toInt: ${s}")
+          s.toInt
+        }
+        val lookup:User => Int => Double  = { user =>
+          logger.info(s"lookup: ${user}")
+          k => k * cache.getOrElse(user.getUserId, 1.0)
+        }
+        val doubleToInt:Double => Int = { d =>
+          logger.info(s"doubleToInt: ${d}")
+          (d * 1000).toInt
+        }
+        val toResult:Int => Result[Int] = { i =>
+          logger.info(s"toResult: ${i}")
+          Result[Int](i)
+        }
+
+        val chain:User => Payload => Result[Int] = u => extractPayload andThen toInt andThen lookup(u) andThen doubleToInt andThen toResult
+        chain
   }
 
 }
