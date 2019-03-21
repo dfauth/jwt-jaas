28c826f46d38ff3f98cb7ab1ca2c273cc7adc8b5
Damian Fauth <dfauth@gmail.com>
2019-03-21 08:32:51 +0800

    recursive jsonFormat

--- kafka/src/test/scala/com/github/dfauth/jwt_jaas/kafka/UserContextSpec.scala
+++ kafka/src/test/scala/com/github/dfauth/jwt_jaas/kafka/UserContextSpec.scala
@@ -8,7 +8,7 @@ import com.typesafe.scalalogging.LazyLogging
 import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
 import org.apache.kafka.common.serialization.{Deserializer, Serializer}
 import org.scalatest.{FlatSpec, Matchers}
-import spray.json.{DefaultJsonProtocol, RootJsonFormat, _}
+import spray.json._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Future, Promise}
@@ -24,13 +24,17 @@ class UserContextSpec
 
   "jsonsupport" should "support round trip" in {
 
+    import JsonSupport._
+
     val payload: Payload[String] = new Payload("test")
-    val bytes = new StringPayloadSerializer().serialize(TOPIC, payload)
-    val result = new StringPayloadDeserializer().deserialize(TOPIC, bytes)
+    val bytes = new PayloadSerializer[String](d => d.toJson).serialize(TOPIC, payload)
+    val result = new PayloadDeserializer[String](o => o.convertTo[Payload[String]]).deserialize(TOPIC, bytes)
     result should be (payload)
   }
 
   "user context" should "be something" in {
+
+    import JsonSupport._
 
     try {
 
@@ -38,14 +42,14 @@ class UserContextSpec
         val (zookeeperConnectString, brokerList) = connectionProperties(config)
 
         val producer = KafkaProducerWrapper[Payload[String]](TOPIC,
-          StringPayloadSerializer(),
+          new PayloadSerializer[String](d => d.toJson),
           brokerList = brokerList,
           zookeeperConnect = zookeeperConnectString,
           props = config.customProducerProperties
         )
 
         val consumer = new KafkaConsumerWrapper[Payload[String]](TOPIC,
-          new StringPayloadDeserializer,
+          new PayloadDeserializer[String](o => o.convertTo[Payload[String]]),
           brokerList = brokerList,
           zookeeperConnect = zookeeperConnectString,
           props = config.customConsumerProperties
@@ -73,20 +77,22 @@ class UserContextSpec
 
   "user context" should "include the user token" in {
 
+    import JsonSupport._
+
     try {
 
       withRunningKafkaOnFoundPort(EmbeddedKafkaConfig(kafkaPort = 9092, zooKeeperPort = 2181)) { implicit config =>
         val (zookeeperConnectString, brokerList) = connectionProperties(config)
 
         val producer = KafkaProducerWrapper[UserContext[Payload[String]]](TOPIC,
-          UserContextSerializer(new StringPayloadSerializer()),
+          UserContextSerializer(new PayloadSerializer[String](d => d.toJson)),
           brokerList = brokerList,
           zookeeperConnect = zookeeperConnectString,
           props = config.customProducerProperties
         )
 
         val consumer = new KafkaConsumerWrapper[UserContext[Payload[String]]](TOPIC,
-          new UserContextDeserializer(new StringPayloadDeserializer()),
+          new UserContextDeserializer(new PayloadDeserializer[String](o => o.convertTo[Payload[String]])),
           brokerList = brokerList,
           zookeeperConnect = zookeeperConnectString,
           props = config.customConsumerProperties
@@ -112,6 +118,38 @@ class UserContextSpec
     }
   }
 
+  "json format" should "allow round trip" in {
+
+    import JsonSupport._
+
+    {
+      val ref = new Payload[String]("hello")
+      val serialized = ref.toJson.prettyPrint.getBytes
+      val result = JsonParser(serialized).asJsObject.convertTo[Payload[String]]
+      result should be (ref)
+    }
+
+    {
+      val ref = new Result[Int](2)
+      val serialized = ref.toJson.prettyPrint.getBytes
+      val result = JsonParser(serialized).asJsObject.convertTo[Result[Int]]
+      result should be (ref)
+    }
+
+    {
+      val ref = new UserContext[Payload[String]]("blah", new Payload[String]("hello"))
+      val serialized = ref.toJson.prettyPrint.getBytes
+      val result = JsonParser(serialized).asJsObject.convertTo[UserContext[Payload[String]]]
+      result should be (ref)
+    }
+
+    {
+      val ref = new Payload[String]("hello")
+      val serialized = new PayloadSerializer[String](d => d.toJson).serialize(TOPIC, ref)
+      val result = new PayloadDeserializer[String](o => o.convertTo[Payload[String]]).deserialize(TOPIC,serialized)
+      result should be (ref)
+    }
+  }
 }
 
 object Utils {
@@ -143,19 +181,20 @@ case class Payload[T](payload:T)
 case class Result[T](result:T)
 
 object JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val usCtxFormat:RootJsonFormat[UserContext[Payload[String]]] = jsonFormat2(UserContext[Payload[String]])
-  implicit val payloadFormat:RootJsonFormat[Payload[String]] = jsonFormat1(Payload[String])
-  implicit val resultFormat:RootJsonFormat[Result[Int]] = jsonFormat1(Result[Int])
+  implicit def usCtxFormat[T:JsonFormat]:RootJsonFormat[UserContext[T]] = jsonFormat2(UserContext.apply[T])
+  implicit def payloadFormat[T:JsonFormat]:RootJsonFormat[Payload[T]] = jsonFormat1(Payload.apply[T])
+  implicit def resultFormat[T:JsonFormat]:RootJsonFormat[Result[T]] = jsonFormat1(Result.apply[T])
 }
 
-case class StringPayloadDeserializer() extends PayloadDeserializer[String] {
-  override def deserialize(topic: String, data: Array[Byte]): Payload[String] = JsonSupport.payloadFormat.read(JsonParser(data).asJsObject)
-}
-
-abstract class PayloadDeserializer[T]() extends Deserializer[Payload[T]] {
+class PayloadDeserializer[T](f:JsValue => Payload[T]) extends Deserializer[Payload[T]] {
   override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {}
 
   override def close(): Unit = {}
+
+  override def deserialize(topic: String, data: Array[Byte]): Payload[T] = {
+    val jsObj: JsValue = JsonParser(data).asJsObject
+    f(jsObj)
+  }
 }
 
 case class UserContextSerializer[T](nested:Serializer[T]) extends Serializer[UserContext[T]] {
@@ -175,13 +214,13 @@ case class UserContextDeserializer[T](nested:Deserializer[T]) extends Deserializ
   override def deserialize(topic: String, data: Array[Byte]): UserContext[T] = ???
 }
 
-case class StringPayloadSerializer() extends PayloadSerializer[String] {
-  override def serialize(topic: String, data: Payload[String]): Array[Byte] = JsonSupport.payloadFormat.write(data).prettyPrint.getBytes
-}
-
-abstract class PayloadSerializer[T]() extends Serializer[Payload[T]] {
+class PayloadSerializer[T](f:Payload[T] => JsValue) extends Serializer[Payload[T]] {
 
   override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = {}
 
   override def close(): Unit = {}
+
+  override def serialize(topic: String, data: Payload[T]): Array[Byte] = {
+    f(data).prettyPrint.getBytes
+  }
 }
