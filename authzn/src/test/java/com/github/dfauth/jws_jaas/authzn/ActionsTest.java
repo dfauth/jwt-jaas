9ab0eb44183045591cfa63ecdfaf8c35e054bfbb
fauthd <damian.fauth@sgx.com>
2019-03-25 17:00:14 +0800

    authzn: unit tests passing

--- authzn/src/test/java/com/github/dfauth/jws_jaas/authzn/ActionsTest.java
+++ authzn/src/test/java/com/github/dfauth/jws_jaas/authzn/ActionsTest.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -16,11 +19,32 @@ public class ActionsTest {
     @Test
     public void testParse() {
 
-        TestPermission perm = new TestPermission("/a/b/c/d/e/f", Actions.using(TestAction.class).parse("*"));
+        {
+            TestPermission perm = new TestPermission("/a/b/c/d/e/f", Actions.using(TestAction.class).parse("*"));
 
-        Stream.of(TestAction.values()).forEach(v -> {
-            assertTrue(perm.getActions().contains(v));
-        });
+            Stream.of(TestAction.values()).forEach(v -> {
+                assertTrue(perm.getActions().contains(v));
+            });
+
+            perm.getActions().stream().forEach(v -> {
+                assertTrue(Arrays.asList(TestAction.values()).contains(v));
+            });
+        }
+
+        {
+            TestPermission perm = new TestPermission("/a/b/c/d/e/f", Actions.using(TestAction.class).parse("creaTE , dElEtE"));
+
+            List<TestAction> actions = Arrays.asList(new TestAction[]{TestAction.DELETE, TestAction.CREATE});
+
+            actions.forEach(v -> {
+                assertTrue(perm.getActions().contains(v));
+            });
+
+
+            perm.getActions().stream().forEach(v -> {
+                assertTrue(actions.contains(v));
+            });
+        }
 
     }
 
