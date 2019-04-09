e5b07c8d9c5f5b7e2563af417577b158e3ef4c6c
Damian Fauth <dfauth@gmail.com>
2019-04-10 05:09:57 +0800

    WebSocketKafkaAdapter: refactor, no dependency on RestEndpointServer

--- src/main/scala/com/github/dfauth/jwt_jaas/WebSocketKafkaAdapter.scala
+++ src/main/scala/com/github/dfauth/jwt_jaas/WebSocketKafkaAdapter.scala
@@ -1,29 +1,24 @@
 package com.github.dfauth.jwt_jaas
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
-import akka.http.scaladsl.server.Directives.{complete, get, handleWebSocketMessages, path}
+import akka.http.scaladsl.server.Directives.{handleWebSocketMessages, path}
 import akka.http.scaladsl.server.Route
 import akka.kafka.scaladsl.Consumer
-import akka.kafka.{ConsumerSettings, ProducerSettings, Subscriptions}
+import akka.kafka.{ConsumerSettings, Subscriptions}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink}
-import com.github.dfauth.jwt_jaas.kafka.{JsValueDeserializer, JsValueSerializer}
-import com.github.dfauth.jwt_jaas.rest.RestEndPointServer
-import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
+import com.github.dfauth.jwt_jaas.kafka.{JsValueDeserializer}
+import com.typesafe.scalalogging.LazyLogging
+import org.apache.kafka.common.serialization.{StringDeserializer}
 import spray.json.JsValue
 
 import scala.concurrent.Future
 
 object WebSocketKafkaAdapter {
 
-  val statusRoute:Route = path("status") {
-    get {
-      complete(HttpEntity(ContentTypes.`application/json`, """{"ok": true}"""))
-    }
-  }
 
 }
 
@@ -31,15 +26,31 @@ class WebSocketKafkaAdapter[T](hostname:String = "localhost",
                                port:Int = 8080,
                                brokerList:String = "localhost:9092",
                                topic:String = "subscribe",
+                               endpoint:String = "subscribe",
                                deserializer:JsValue => T,
-                               serializer:T => JsValue) extends RestEndPointServer(WebSocketKafkaAdapter.statusRoute, hostname, port) {
+                               serializer:T => JsValue) extends LazyLogging {
+
+  implicit val system = ActorSystem("webSocketKafkaAdapter")
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
 
   val websocketRoute:Route =
-    path("subscribe") {
+    path(endpoint) {
       handleWebSocketMessages(subscribeFlow(brokerList))
     }
 
-  def start():Future[ServerBinding] = start(websocketRoute)
+  def start():Future[ServerBinding] = {
+    Http().bindAndHandle(websocketRoute, hostname, port)
+  }
+
+  def stop(bindingFuture:Future[ServerBinding]):Unit = {
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => {
+      system.terminate()
+      logger.info("system terminated")
+    }) // and shutdown when done
+  }
 
   def subscribeFlow(brokerList:String): Flow[Message, Message, Any] = {
 
@@ -59,8 +70,4 @@ class WebSocketKafkaAdapter[T](hostname:String = "localhost",
       logger.info(s"subscribeFlow: ${i}")
     }), source)
   }
-
-
-
 }
-
